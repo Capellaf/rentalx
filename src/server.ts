679d050c66572ac2f2297e6001c053aba0024616
Capellaf <filipecapella@gmail.com>
2021-05-07 18:56:28 -0300

    setup remolded

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser"
 import { categoriesRoutes } from "./routes/category.routes";
+import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/categories", categoriesRoutes);
+app.use("/specifications", specificationsRoutes);
 
 app.listen(3333, () => { console.log("Server is running!") })
