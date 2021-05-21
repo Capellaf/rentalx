efc01c22151e3a4e7de8593759bfd4233bcbef7b
Capellaf <filipecapella@gmail.com>
2021-05-21 20:24:29 -0300

    upload and documentation

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,11 +1,8 @@
 import { Specification } from "../model/Specification";
-
-
 interface ICreateSpecificationDTO {
     name: string;
     description: string;
 }
-
 interface ISpecificationsRepository {
     create({ description, name }: ICreateSpecificationDTO): void;
     findByName(name: string): Specification;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express"
 import bodyParser from "body-parser"
 import { router } from "./routes";
+
+const swaggerFile = require("./swagger.json");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(router);
 
 app.listen(3333, () => { console.log("Server is running!") })
